72c44fccbc3b93f394001716b394f8901baf49a1
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-04 11:01:32 +0530

    Create Footer.jsx

---
+++ src/components/frontend/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      {/* Footer content */}
+      Footer Section
+    </footer>
+  );
+};
+
+export default Footer;

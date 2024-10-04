11698691c0fef020cc0f934bb9f3bbc5c68e2007
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-04 11:01:26 +0530

    Create Header.jsx

---
+++ src/components/frontend/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      {/* Header content */}
+      Header Section
+    </header>
+  );
+};
+
+export default Header;

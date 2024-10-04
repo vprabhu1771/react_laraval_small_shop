432fde662b452131aa737de696ac4a752428723a
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-04 11:12:38 +0530

    Update TopBar.jsx

--- src/components/frontend/layout/TopBar.jsx
+++ src/components/frontend/layout/TopBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopBar = () => {
   return (
-    <div>
-      {/* Top bar content */}
-      Top Bar
-    </div>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+      <Link to="/cart">Cart</Link>
+      <button>Logout</button>
+    </nav>
   );
 };
 

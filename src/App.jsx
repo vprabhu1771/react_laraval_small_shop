60dfa6827d752243bc07095ac13c134f19bd01a8
vprabhu1771 <vprabhu1771@gmail.com>
2024-10-04 11:13:04 +0530

    Update App.jsx Login, Register, Cart Routes added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AppLayout from './components/frontend/layout/AppLayout';
 import Home from './components/frontend/Home';
+import Login from './components/frontend/auth/Login';
+import Register from './components/frontend/auth/Register';
+import Cart from './components/frontend/Cart';
 
 function App() {
   
@@ -13,6 +16,12 @@ function App() {
         <Routes>
 
           <Route path='/' element={<Home />} />
+
+          <Route path='/login' element={<Login />} />
+
+          <Route path='/register' element={<Register />} />
+
+          <Route path='/cart' element={<Cart />} />
 
         </Routes>
 
